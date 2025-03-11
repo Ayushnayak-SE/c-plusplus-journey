b49ca427ef47502cef713477152c01414eb27bd8
Ayush Nayak <ayushnayak511@gmail.com>
2025-03-11 11:53:14 +0530

    Add files via upload

---
+++ simpleinterest.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){//simple interest
+    float p = 1000, r = 20 , t = 5;
+    float SI = p*r*t/100;//si formula
+    cout<<"Simple interest is :"<<SI<<endl;
+}
---
+++ typecasting.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"input :";
+    cin>>x;
+    float y = (float)x;    //typecasting
+    cout<<"half is :"<<y/2;
+}
