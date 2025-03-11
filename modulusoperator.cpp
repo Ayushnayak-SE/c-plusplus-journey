ec4cac3926826facc79ade6412ac3b9aaca2046e
Ayush Nayak <ayushnayak511@gmail.com>
2025-03-11 11:53:59 +0530

    Add files via upload

---
+++ modulusoperator.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"enter value of x :";
+    cin>>x;
+    cout<<"enter value of y :";
+    cin>>y;
+    cout<<"modulus ="<<x%y;
+}
