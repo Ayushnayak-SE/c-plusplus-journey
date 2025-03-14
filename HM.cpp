4d9d6895571a68abf61b568c19062d88a36ca661
Ayush Nayak <ayushnayak511@gmail.com>
2025-03-14 13:49:18 +0530

    Add files via upload

---
+++ HM.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a;
+    a = 7.0/22.0*(3.14+2.0)*3.0/5.0;
+    cout<<"a ="<<a;
+}
---
+++ inrement.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=4;
+    cout<<x<<endl;//4
+    cout<<--x<<endl;//3
+    cout<<x<<endl;//3
+    cout<<x++<<endl;//3
+    cout<<x<<endl;//4
+    cout<<++x<<endl;//5
+    cout<<x<<endl;//5
+}
