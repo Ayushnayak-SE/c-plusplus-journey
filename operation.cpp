b02400e0456d1d2d47822377b625a921a8934be4
Ayush Nayak <ayushnayak511@gmail.com>
2025-03-18 10:59:43 +0530

    Add files via upload

---
+++ operation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    cout<<x<<endl;
+     x = 6; //updation
+    cout<<x<<endl;
+    x=x+6;  //operation with new data 6
+    cout<<x<<endl;
+    x=x-8;  //operation with 12
+    cout<<x<<endl;
+}
