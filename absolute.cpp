0e2ff286c265c91df41bf82940f322ac59a9f8a9
Ayush Nayak <ayushnayak511@gmail.com>
2025-03-19 15:06:58 +0530

    Add files via upload

---
+++ absolute.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a value";
+    cin>>n;
+    if(n>=0)
+    cout<<"absolute value is :"<<n;
+    else 
+    cout<<"absolute value is :"<<-n;
+}
