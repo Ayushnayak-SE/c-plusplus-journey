420a7a4cb1993b08497eeb7a2097e33d06d51e68
Ayush Nayak <ayushnayak511@gmail.com>
2025-03-23 23:55:18 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter a value :";
+    cin>>a;
+    if(a%2 == 0)
+    cout<<"number is even";
+    else
+    {cout<<"number is odd"<<endl;
+        cout<<"wow soo beautiful";
+}
+}
