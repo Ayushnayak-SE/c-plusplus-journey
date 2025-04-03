2ab56f51f27ad2a2cdc293a4aa4371be73bcb820
Ayush Nayak <ayushnayak511@gmail.com>
2025-04-03 11:25:40 +0530

    Add files via upload

---
+++ threedigit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number :";
+    cin>>n;
+    if(n>99 && n<1000)
+    cout<<"It is a three digit number !!!";
+    if(n<99 && n>9)
+    cout<<"it is a two digit number !!!";
+
+    
+}
